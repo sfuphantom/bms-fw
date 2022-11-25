4e9181c95e0811d7821bbfb75af88ef0016bab67
LONA-LAPTOP\lonam <lml26@sfu.ca>
2022-11-24 19:31:07 -0800

    implement basic cli response

--- BMS Workspace-git/bms-1227-fw/Phantom_drivers/rtos/source/task_command_line.c
+++ BMS Workspace-git/bms-1227-fw/Phantom_drivers/rtos/source/task_command_line.c
@@ -13,20 +13,29 @@
 #include "Phantom_sci.h"
 #include "hwConfig.h"
 
+static uint8 previous;
 static xTaskHandle commandLineTaskHandle;
 static void CommandLineTask(void* params){
     while(1){
-        UARTprintf("Hello World!\r\n");
+        uint8 msg;
+        sciReceive(PC_UART, 1, &msg);
+        if(msg==previous){
+            // do nothing
+        } else if(msg=='1'){
+            UARTprintf("Message 1 received\r\n");
+        } else if(msg=='2'){
+            UARTprintf("Message 2 received\r\n");
+        } else if(msg=='3'){
+            UARTprintf("Message 3 received\r\n");
+        }
+        previous = msg;
         vTaskDelay(1000);
-
     }
 }
-
 
 void initializeCommandLine()
 {
         UARTInit(PC_UART, 9600);
-
 
     xTaskCreate(CommandLineTask, "CommandLineTask", configMINIMAL_STACK_SIZE, ( void * ) 1, tskIDLE_PRIORITY, &commandLineTaskHandle);
 }
